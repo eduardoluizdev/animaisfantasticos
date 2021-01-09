3ea9b5a5dd448843d9f5ce094520ff19b5d5e2cb
Eduardo Luiz <hello@eduardoluiz.dev>
2021-01-09 18:53:15 -0300

    Add return this on classes refactored

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -3,28 +3,41 @@ export default class Modal {
     this.openButton = document.querySelector(open);
     this.closeButton = document.querySelector(close);
     this.containerModal = document.querySelector(container);
+
+    // bind this ao callback para
+    // fazer referência ao objeto
+    // da classe
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.clickOutModal = this.clickOutModal.bind(this);
   }
 
-  toggleModal(e) {
-    e.preventDefault();
+  // abre ou fecha o modal
+  toggleModal() {
     this.containerModal.classList.toggle('ativo');
   }
 
+  // adiciona o evento de toggle ao modal
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
+  }
+
   clickOutModal(e) {
-    if (e.target === this) {
-      this.toggleModal(e);
+    if (e.target === this.containerModal) {
+      this.toggleModal();
     }
   }
 
-  addModalEvent() {
+  addModalEvents() {
     this.openButton.addEventListener('click', this.toggleModal);
     this.closeButton.addEventListener('click', this.toggleModal);
     this.containerModal.addEventListener('click', this.clickOutModal);
   }
 
   init() {
-    if (this.openButton && this.closeButton && this.containerModal) {
-      this.addModalEvent();
+    if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+      this.addModalEvents();
     }
+    return this;
   }
 }
